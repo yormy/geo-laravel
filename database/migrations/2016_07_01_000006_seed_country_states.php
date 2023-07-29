b213c8d3eff24a3ec2c7f07a50bf6235be536ac8
Mexion <dev@mexion.com>
2023-07-29 16:02:26 +0200

    seed geo

--- database/migrations/2016_07_01_000006_seed_country_states.php
+++ database/migrations/2016_07_01_000006_seed_country_states.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {    public function up()
     {
-        $path = 'database/seeders/Locations/Data/states.sql';
+        $path = __DIR__ . '../../seeders/Locations/Data/states.sql';
         $sql = file_get_contents($path);
         DB::unprepared($sql);
     }
--- database/migrations/2016_07_01_000007_seed_timezones.php
+++ database/migrations/2016_07_01_000007_seed_timezones.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\DB;
 return new class extends Migration
 {    public function up()
     {
-        $path = 'database/seeders/Locations/Data/timezones.sql';
+        $path = __DIR__ . '../../seeders/Locations/Data/timezones.sql';
         $sql = file_get_contents($path);
         DB::unprepared($sql);
     }
